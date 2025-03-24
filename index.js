57e440f81ae81bc3b7f35f899ef9a66940681b0a
Luke <luke.wangai@student.moringaschool.com>
2025-03-24 09:35:49 +0300

    iterators complete

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+
+
+
+
+
+function superbowlWin(record){
+  const win = record.find(record => record.result === "W")
+  if(win){
+    return win.year
+  } else {
+    undefined
+  }
+}
+    
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"}
+]
+record.find(superbowlWin)
